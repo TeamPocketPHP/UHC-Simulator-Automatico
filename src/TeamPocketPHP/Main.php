18f6c1a9e0fa9e8135631847e8a00004e5bbf0f5
CreeperZonyck <juancali-28@hotmail.com>
2017-02-05 23:03:07 -0500

    Add files via upload

---
+++ src/TeamPocketPHP/Main.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TeamPocketPHP;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase {
+	
+		
+	public function onEnable() {
+		
+		  $this->getLogger()->info("UHC Simulator Activado");
+		
+		
+	}
+	
+	
+}
